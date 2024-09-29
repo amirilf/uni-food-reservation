4ff914a5ab7e3532f1d4825b08337ee49090087f
Amir Mohammad Khorasani <amirilf@protonmail.com>
2024-09-29 21:18:39 +0330

    Complete captcha recognition using a very shity method

--- core/captcha/analyze.py
+++ core/captcha/analyze.py
@@ -1,7 +1,7 @@
 from PIL import Image
+from math import sqrt
 from utility.hash import get_img_hash_by_object
 from utility.file import create_dir
-
 
 DIGIT_REGIONS = [
     (0, 17), # first digit is from col 0 to 17
@@ -10,7 +10,7 @@ DIGIT_REGIONS = [
     (60, 77),
 ]
 
-def extract_digit_regions(image_path: str) -> list[Image.Image]:
+def extract_digit_regions_from_captcha_image(image_path: str) -> list[Image.Image]:
     
     img = Image.open(image_path)
     img = img.convert('RGB')
@@ -59,7 +59,7 @@ def calculate_white_pixel_density(image: Image.Image) -> float:
 
 def analyze_image(image_path: str) -> list[float]:
 
-    digit_regions = extract_digit_regions(image_path)    
+    digit_regions = extract_digit_regions_from_captcha_image(image_path)    
     all_densities = []
     
     for digit_region in digit_regions:
@@ -69,36 +69,188 @@ def analyze_image(image_path: str) -> list[float]:
     
     return all_densities
 
-def print_image_data(image_path : str) -> None:
-    
-    densities = analyze_image(image_path)
+def print_densites_like_matrix(density : list[float]) -> None:
     for row in range(5):
         for col in range(6):
-            print(f"{densities[0][(row * 6) + col]:.2f}", end=" | ")
+            print(f"{density[(row * 6) + col]:.2f}", end=" | ")
         print()
 
+def seprate_digits_in_dirs():
+    hashes = [[],[],[],[]]
+    counter = 1
 
-# TEST
-hashes = [[],[],[],[]]
-counter = 1
+    create_dir("tmp/digits/1",False)
+    create_dir("tmp/digits/2",False)
+    create_dir("tmp/digits/3",False)
+    create_dir("tmp/digits/4",False)
 
-create_dir("tmp/digits/1",False)
-create_dir("tmp/digits/2",False)
-create_dir("tmp/digits/3",False)
-create_dir("tmp/digits/4",False)
+    for i in range(1,1001):
+        path = "../tmp/images/" + str(i) + ".png"
+        img = extract_digit_regions_from_captcha_image(path)
+        
+        for j in range(4):
 
-for i in range(1,20):
-    path = "tmp/images/" + str(i) + ".png"
-    img = extract_digit_regions(path)
+            img_hash = get_img_hash_by_object(img[j])
+            if img_hash not in hashes[j]:
+                save_path = f"tmp/digits/{j+1}/{counter}.png"
+                img[j].save(save_path, format="PNG")
+                counter += 1
+                hashes[j].append(img_hash)
+                print(i,j+1,"saved!")
+            else:
+                print(i,j+1,"duplicate found!")
+
+def get_digit_densities_list(image_path : str) -> list[float]:
+    parts = split_digit_into_parts(Image.open(image_path))
+    densities = [round(calculate_white_pixel_density(part) * 100) for part in parts]
+    return densities
+
+def find_first_digit(image_densities : list[float]) -> int:    
     
-    for j in range(4):
-
-        img_hash = get_img_hash_by_object(img[j])
-        if img_hash not in hashes[j]:
-            save_path = f"tmp/digits/{j+1}/{counter}.png"
-            img[j].save(save_path, format="PNG")
-            counter += 1
-            hashes[j].append(img_hash)
-            print(i,j+1,"saved!")
+    if image_densities[24]:
+        # 2,3,5,6,8,9
+        if not image_densities[9]:
+            # 2,9 {maybe 8}
+            if not image_densities[29]:
+                # 9 {maybe 8}
+                if image_densities[18] > 0.3:
+                    return 8
+                return 9
+            return 2
         else:
-            print(i,j+1,"duplicate found!")
+            # is 3,5,6,8
+            if image_densities[12]:
+                # 5,6,8
+                if image_densities[17]:
+                    # 5,6
+                    if image_densities[10]:
+                        return 6
+                    return 5
+                return 8  
+            return 3     
+    else:
+        # 1,4,7
+        if image_densities[4]:
+            # 4,7
+            if image_densities[12]:
+                return 4
+            return 7
+        return 1
+
+def find_second_digit(image_densities : list[float]) -> int:
+    
+    if image_densities[24]:
+        # 2,3,5,8,9
+        if image_densities[11]:
+            # 2,3,8,9   
+            if image_densities[13]:
+                # 8,9
+                if image_densities[18]:
+                    return 8
+                return 9
+            else:
+                # 2,3
+                if image_densities[23]:
+                    return 3
+                return 2
+        return 5
+    else:
+        # 0,1,4,6,7
+        if image_densities[14]:
+            # 4,6
+            if image_densities[21]:
+                return 4
+            return 6
+        else:
+            # 0,1,7
+            if image_densities[5]:
+                # 0,7
+                if image_densities[21]:
+                    return 7
+                return 0
+            else:
+                return 1
+
+def find_third_digit(image_densities : list[float]) -> int:
+    if image_densities[0]:
+        # 0,2,3,5,6,7,8,9
+        if image_densities[18]:
+            # 0,3,5,6,8,9
+            if image_densities[12]:
+                # 0,5,6,9
+                if image_densities[9]:
+                    # 5,6
+                    if image_densities[10]:
+                        return 6
+                    return 5
+                else:
+                    # 0,9
+                    if image_densities[14]:
+                        return 9
+                    return 0
+            else:
+                # 3,8
+                if image_densities[13]:
+                    return 8
+                return 3
+        else:
+            # 2,7
+            if image_densities[28]:
+                return 2
+            return 7
+    else:
+        # 1,4
+        if image_densities[19]:
+            return 4
+        return 1
+
+def find_forth_digit(image_densities : list[float]) -> int:
+    
+    if image_densities[24]:
+        # 2,3,5,8,9
+        if image_densities[13]:
+            # 5,8,9
+            if image_densities[11]:
+                # 8,9
+                if image_densities[18]:
+                    return 8
+                return 9
+            return 5
+        else:
+            # 2,3
+            if image_densities[23]:
+                return 3
+            return 2
+    else:
+        # 0,1,3,4,6,7
+        if image_densities[13]:
+            # 0,4,6
+            if not image_densities[21]:
+                # 0,6
+                if image_densities[14]:
+                    return 6
+                return 0
+            return 4
+        else:
+            # 1,3,7
+            if image_densities[2]:
+                # 3,7
+                if image_densities[20]:
+                    return 7
+                return 3
+            return 1
+
+# Test
+list_of_densities = []
+for i in range(1,300):
+    path = f"tmp/digits/4/{i}.png"
+    try:
+        densities = get_digit_densities_list(path)
+        list_of_densities.append([i,densities])
+    except Exception as e:
+        pass
+
+print('==================')
+
+for i in list_of_densities:
+    print(i[0],find_forth_digit(i[1]))
